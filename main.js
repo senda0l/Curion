2f853a6bab538e9c3cd7d9be12038b4a2f1c5741
senda0l <hairyfobbit@gmail.com>
2025-07-15 01:12:29 +0500

    Update main.js

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ form.addEventListener('submit', async (e) => {
   }
 
   try {
-    const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`);
+    const res = await fetch(`https://api.exchangerate.host/latest?amount=${amount}&from=${from}&to=${to}`);
     const data = await res.json();
     const rate = data.rates[to];
 
